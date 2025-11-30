401f92fae80bc0f55a3ca6255d7830c528c4f611
Victor Schweitz <victorschweitz@hotmail.com>
2025-11-30 17:01:27 +0100

    Extracted authentication into its own reusable function

--- src/js/supabase/supabase_methods.js
+++ src/js/supabase/supabase_methods.js
@@ -1,3 +1,23 @@
+import { supabaseClient } from "./supabase_client"
+
+// Initialize user
+export const initializeAuth = async (onAuthChange) =>
+{
+    // Check initial auth state
+    const {data: {user}} = await supabaseClient.auth.getUser()
+
+    let isLoggedIn = !!user
+
+   // Call the callback with initial state
+   onAuthChange(isLoggedIn)
+
+   supabaseClient.auth.onAuthStateChange((event, session) => {  // Fixed: onAuthStateChange
+    isLoggedIn = !!session?.user
+    console.log('Auth state changed: ', isLoggedIn)
+    onAuthChange(isLoggedIn)
+   })
+}
+
 // Sign in
 export const signIn = async (email, password) =>
 {
