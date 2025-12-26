aaf248df282965b21b51f9f8d90a690ba2fc8582
Victor Schweitz <victorschweitz@hotmail.com>
2025-12-26 12:03:14 +0100

    Dialogs fixed

--- src/js/utils/form_config.js
+++ src/js/utils/form_config.js
@@ -1,8 +1,55 @@
 export const formVariants = {
-   sign_in: {
-    headingText: "Sign up",
-    buttonText: "Sign up",
-    inputs: [
+   sign_up:
+   {
+      headingText: "Sign up",
+      buttonText: "Sign up",
+      inputs: [
+        {
+          type: "email",
+          labelText: "E-mail",
+          placeholder: "Enter e-mail",
+          name: "email_input",
+        },
+        {
+          type: "text",
+          labelText: "Name",
+          placeholder: "Enter full name",
+          name: "full_name_input",
+        },
+        {
+          type: "password",
+          labelText: "Password",
+          placeholder: "Enter password",
+          name: "password_input",
+        },
+        {
+          type: "password",
+          labelText: "Repeat Password",
+          placeholder: "Repeat password",
+          name: "repeat_password_input",
+        },
+        {
+          type: "file",
+          labelText: "Profile image",
+          name: "avatar_image_upload_input",
+        },
+      ],
+
+      button:
+      {
+        text: 'Sign up',
+        variant: 'form_button',
+        type: 'submit'
+      }
+    },
+
+  login:
+  {
+    headingText: 'Log In',
+    buttonText: 'Log In',
+
+    inputs:
+    [
       {
         type: "email",
         labelText: "E-mail",
@@ -10,54 +57,21 @@ export const formVariants = {
         name: "email_input",
       },
       {
-        type: "text",
-        labelText: "Name",
-        placeholder: "Enter full name",
-        name: "full_name_input",
-      },
-      {
         type: "password",
         labelText: "Password",
         placeholder: "Enter password",
         name: "password_input",
       },
-      {
-        type: "password",
-        labelText: "Repeat Password",
-        placeholder: "Repeat password",
-        name: "repeat_password_input",
-      },
-      {
-        type: "file",
-        labelText: "Profile image",
-        name: "avatar_image_upload_input",
-      },
     ],
-  },
 
-  login:
-  {
-    headingText: 'Log In',
-    buttonText: 'Log In',
-
-    inputs: [
+    button:
       {
-    type: "email",
-    labelText: "E-mail",
-    placeholder: "Enter e-mail",
-    name: "email_input",
+        text: 'Log In',
+        variant: 'form_button',
+        type: 'submit'
+      }
   },
-  {
-    type: "password",
-    labelText: "Password",
-    placeholder: "Enter password",
-    name: "password_input",
-  },
-    ]
-  },
-  comment:
-  {
-    headingText: 'Comment',
-    buttonText: 'Comment',
-  }
+ 
+
+
 }
