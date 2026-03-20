c37ed02a4def8e5b992607f568bd9027195953ec
Victor Schweitz <victorschweitz@hotmail.com>
2026-03-20 21:48:05 +0100

    Added tailwindcss() to the plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
   output: 'server',
   adapter: netlify(),
 });
